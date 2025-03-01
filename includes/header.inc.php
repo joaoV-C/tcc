14311dc05f52c6ceaed11af025ca26550fee1fb6
joaoV-C <joaov.costacardoso@gmail.com>
2025-03-01 15:01:24 +0000

    header include file

---
+++ includes/header.inc.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="logo">PlaceHolder</div>
+
+    <!-- Checkbox para controlar o menu no mobile -->
+    <input type="checkbox" id="menu-toggle" />
+    <label for="menu-toggle" class="menu-icon">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+
+    <nav>
+      <ul>
+        <li><a href="#">Trabalhos</a></li>
+        <li><a href="#">Artistas</a></li>
+        <li><a href="#">Sobre a Loja</a></li>
+        <li><a href="#">Compre</a></li>
+        <li><a href="#">Login</a></li>
+      </ul>
+    </nav>
+  </header>
