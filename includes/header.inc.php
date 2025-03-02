af7e0a625d5a9b69a8e35ea5382c1d2ff069f9d9
joaoV-C <joaov.costacardoso@gmail.com>
2025-03-02 13:31:37 +0000

    Add a link to the main page to the logo

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,21 +1,22 @@
 <header>
-    <div class="logo">PlaceHolder</div>
+  <div class="logo"><a href="index.php">PlaceHolder</a></div>
 
-    <!-- Checkbox para controlar o menu no mobile -->
-    <input type="checkbox" id="menu-toggle" />
-    <label for="menu-toggle" class="menu-icon">
-      <span></span>
-      <span></span>
-      <span></span>
-    </label>
+  <!-- Checkbox para controlar o menu no mobile -->
+  <input type="checkbox" id="menu-toggle" />
+  <label for="menu-toggle" class="menu-icon">
+    <span></span>
+    <span></span>
+    <span></span>
+  </label>
 
-    <nav>
-      <ul>
-        <li><a href="trabalhos.php">Trabalhos</a></li>
-        <li><a href="artistas.php">Artistas</a></li>
-        <li><a href="sobre.php">Sobre a Loja</a></li>
-        <li><a href="compre.php">Compre</a></li>
-        <li><button>Log in</button></li>
-      </ul>
-    </nav>
-  </header>
+  <nav>
+    <ul>
+      <li><a href="trabalhos.php">Trabalhos</a></li>
+      <li><a href="artistas.php">Artistas</a></li>
+      <li><a href="sobre.php">Sobre a Loja</a></li>
+      <li><a href="compre.php">Compre</a></li>
+      <li><button type="submit">Log in</button></li>
+    </ul>
+  </nav>
+</header>
+<?php
