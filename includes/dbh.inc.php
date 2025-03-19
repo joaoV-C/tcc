2ddb36fdbf715ffa98425f3371e031c22980ccb1
joaoV-C <joaov.costacardoso@gmail.com>
2025-03-19 11:15:13 +0000

    Add PDO config to disable emulated prepared statements

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -7,6 +7,7 @@ $dbpassword = '';
 
 try {
   $pdo = new PDO("mysql: host=$host;dbname=$dbname", $dbusername, $dbpassword);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   die("Connection failed: " . $e->getMessage());
