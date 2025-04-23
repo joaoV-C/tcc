56510fb5e0d38b272da4b0077322bd8e08bc12e3
joaoV-C <joaov.costacardoso@gmail.com>
2025-04-23 23:31:50 +0100

    Change html tags

--- src/views/signin_view.inc.php
+++ src/views/signin_view.inc.php
@@ -11,45 +11,37 @@
 <body>
   <?php include('includes/header.inc.php'); ?>
 
-  <div class="signin-container">
-    <form action="/tcc/signin" method="post">
-      <div class="signin-content">
+  <form action="/tcc/signin" method="post" class="form signin-form">
+    <div class="signin-content">
 
-        <h2>Inicie sessão</h2>
+      <h2>Inicie sessão</h2>
 
-        <?php if (isset($_SESSION['user_id'])): ?>
-          <div class="success-message container">
-            <h3><?php echo $signinSuccess ?></h3>
-          </div>
-        <?php endif ?>
+      <?php if (isset($_SESSION['user_id'])): ?>
+        <h3 class="success success-message"><?php echo $signinSuccess ?></h3>
+      <?php endif ?>
 
-        <div class="form-input container" id="signin-email">
-          <input type="text" name="email" placeholder="Email" class="form-textbox-input"
-            value="<?php echo htmlspecialchars($signinData['email'] ?? ''); ?>">
+      <div class="container signin-input-container" id="signin-email">
+        <input type="text" name="email" placeholder="Email" class="form-textbox-input"
+          value="<?php echo htmlspecialchars($signinData['email'] ?? ''); ?>">
 
-          <div class="form-message-wrapper">
-            <?php if (!empty($signinErrors['unsigned_email'])): ?>
-              <p class="error-message"><?php echo $signinErrors['unsigned_email']; ?></p>
-            <?php endif; ?>
-          </div>
-        </div>
-
-        <div class="form-input container" id="signin-password">
-          <input type="password" name="password" placeholder="Palavra-passe" class="form-textbox-input"
-            value="<?php echo htmlspecialchars($signinData['password'] ?? ''); ?>">
-
-          <div class="form-message-wrapper">
-            <?php if (!empty($signinErrors['wrong_password'])): ?>
-              <p class="error-message"><?php echo $signinErrors['wrong_password']; ?></p>
-            <?php endif; ?>
-          </div>
-        </div>
-
-        <input type="hidden" name="signin" value="1">
-        <button type="submit" class="signin btn">Continuar</button>
+        <?php if (!empty($signinErrors['unsigned_email'])): ?>
+          <p class="error error-message"><?php echo $signinErrors['unsigned_email']; ?></p>
+        <?php endif; ?>
       </div>
-    </form>
-  </div>
+
+      <div class="container signin-input-container" id="signin-password">
+        <input type="password" name="password" placeholder="Palavra-passe" class="form-textbox-input"
+          value="<?php echo htmlspecialchars($signinData['password'] ?? ''); ?>">
+
+        <?php if (!empty($signinErrors['wrong_password'])): ?>
+          <p class="error error-message"><?php echo $signinErrors['wrong_password']; ?></p>
+        <?php endif; ?>
+      </div>
+
+      <input type="hidden" name="signin" value="1">
+      <button type="submit" class="signin btn">Continuar</button>
+    </div>
+  </form>
   <div>
     <a href="/tcc/signup">Criar conta</a>
   </div>
